3dd58c1844a4823f9c0ca0b48d59594ad5cab5f3
Peter Robinson <peter.robinson@usask.ca>
2025-04-06 12:01:10 -0600

    Add files via upload

---
+++ Source transcripts/Ha3/introduction.js
@@ -0,0 +1,4 @@
+titlepage='<h3>The Wife of Bath\'s Prologue</h3>
+<p> The Canterbury Tales Project</p>
+<p> Prototype online version, September 2019</p>
+<p>Prepared by Peter Robinson and Barbara Bordalejo</p>'
